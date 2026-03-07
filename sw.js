7ed0822425f54470cd4a398c8c2380387f068b15
a-scene-tence <skynjy050@gmail.com>
2026-03-07 21:12:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
-const CACHE = 'checkmate-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json',
-  '/icon-192.png', '/icon-512.png'];
+const CACHE = 'donworry-v1';
+const ASSETS = [
+  '/donworry/',
+  '/donworry/index.html',
+  '/donworry/manifest.json',
+  '/donworry/icon-192.png',
+  '/donworry/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -24,7 +29,7 @@ self.addEventListener('fetch', e => {
           caches.open(CACHE).then(c => c.put(e.request, clone));
         }
         return res;
-      }).catch(() => caches.match('/index.html'));
+      }).catch(() => caches.match('/donworry/index.html'));
     })
   );
 });
